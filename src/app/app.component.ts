072b5ccaba12d31659103356db79e71ba331f33f
Dizaster0 <a.gangapersaud@gmail.com>
2024-03-01 14:46:13 -0500

    typing challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'type';
+  
+  isWin = false;
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onInput(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    } 
+
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+
 }
